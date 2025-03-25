00840e5f4169a2bcdfc963c5b21503d37585daaf
Mathéo Guilloux <matheo.guilloux@gmail.com>
2025-03-25 08:12:21 +0100

    Fix: ajout des en-têtes de sécurité et .nojekyll

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/portfolio/',
   plugins: [vue()],
+  server: {
+    headers: {
+      'Permissions-Policy': 'interest-cohort=()',
+      'X-Frame-Options': 'DENY'
+    }
+  }
 })
